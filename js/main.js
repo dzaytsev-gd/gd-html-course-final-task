82dd9059aa2849108c1cd40fe32f986388e9fd94
Dmitry Zaytsev <DmitryZajcev@gmail.com>
2017-07-17 21:50:36 +0400

    Ready product admin

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var toggleSuccessPurchasedWindow = function() {
+	$('.modal.purchased-successfully').toggleClass('active');
+}
+
+var toggleEditWindow = function() {
+	$('.modal.edit').toggleClass('active');
+}
+
+var toggleDeleteWindow = function() {
+	$('.modal.delete').toggleClass('active');
+}
