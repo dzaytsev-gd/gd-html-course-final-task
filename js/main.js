27ebe462596513bea90ef2f1137b8f0ae44c2656
Dmitry Zaytsev <DmitryZajcev@gmail.com>
2017-07-24 15:31:21 +0400

    onclick events now set in main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-var toggleSuccessPurchasedWindow = function() {
-	$('.modal.purchased-successfully').toggleClass('active');
-}
+$(function() {
+	var toggleSuccessPurchasedWindow = function() {
+		$('.modal.purchased-successfully').toggleClass('active');
+	}
 
-var toggleEditWindow = function() {
-	$('.modal.edit').toggleClass('active');
-}
+	var toggleEditWindow = function() {
+		$('.modal.edit').toggleClass('active');
+	}
 
-var toggleDeleteWindow = function() {
-	$('.modal.delete').toggleClass('active');
-}
+	var toggleDeleteWindow = function() {
+		$('.modal.delete').toggleClass('active');
+	}
+
+	$('#buy-product-button').click(toggleSuccessPurchasedWindow);
+	$('#buy-product-ok-button').click(toggleSuccessPurchasedWindow);
+
+	$('#edit-product-button').click(toggleEditWindow);
+	$('#edit-product-cancel-button').click(toggleEditWindow);
+	$('#edit-product-submit-button').click(toggleEditWindow);
+
+	$('#delete-product-button').click(toggleDeleteWindow);
+	$('#delete-product-cancel-button').click(toggleDeleteWindow);
+	$('#delete-product-submit-button').click(toggleDeleteWindow);
+});
